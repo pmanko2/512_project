5279b73df8310acdb62b76cda77e035fad3b8d10
Pawel Mankowski <pawelman@gmail.com>
2013-11-06 23:24:00 -0500

    Analysis client added

--- src/Client/AnalysisClient.java
+++ src/Client/AnalysisClient.java
@@ -1,7 +1,6 @@
 package Client;
 
 import ResInterface.*;
-import TransactionManager.TransactionAbortedException;
 
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
@@ -10,21 +9,19 @@ import java.util.*;
 import java.io.*;
 
     
-public class Client
+public class AnalysisClient
 {
     static String message = "blank";
     //this references a Middleware Object
     static ResourceManager rm = null;
-    private static int CURRENT_TRXN;
 
-	@SuppressWarnings("unchecked")
-	public static void main(String args[]) throws TransactionAbortedException
+    @SuppressWarnings({ "rawtypes", "unchecked", "unused", "fallthrough"})
+	public static void main(String args[])
     {
         Client obj = new Client();
         BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
         String command = "";
-        @SuppressWarnings("rawtypes")
-		Vector arguments  = new Vector();
+        Vector arguments  = new Vector();
         int Id, Cid;
         int flightNum;
         int flightPrice;
@@ -35,6 +32,8 @@ public class Client
         int numRooms;
         int numCars;
         String location;
+        String numIterationsString = "0";
+        int NUM_TRANSACTIONS;
 
 
         String server = "teaching.cs.mcgill.ca";
@@ -82,28 +81,28 @@ public class Client
         if (System.getSecurityManager() == null) {
             System.setSecurityManager(new RMISecurityManager());
         }
-
         
+        boolean correctInput = false;
         
-        
-        System.out.println("\n\n\tClient Interface");
-        System.out.println("Type \"help\" for list of supported commands");
-        while(true){
-        System.out.print("\n>");
-        try{
-            //read the next command
-            command =stdin.readLine();
+        while(!correctInput)
+        {
+        	 System.out.println("How many transactions would you like the client to submit?\n>");
+             try {
+     			numIterationsString = stdin.readLine();
+     			NUM_TRANSACTIONS = Integer.parseInt(numIterationsString);
+     			correctInput = true;
+     		} catch (IOException e1) {
+     			// TODO Auto-generated catch block
+     			e1.printStackTrace();
+     		} catch(NumberFormatException notNumber){
+     			correctInput = false;
+     		}
         }
-        catch (IOException io){
-            System.out.println("Unable to read from standard in");
-            System.exit(1);
-        }
-        //remove heading and trailing white space
-        command=command.trim();
-        arguments=obj.parse(command);
+        
+        Random random = new Random();
         
         //decide which of the commands this was
-        switch(obj.findChoice((String)arguments.elementAt(0))){
+        switch(){
         case 1: //help section
             if(arguments.size()==1)   //command was "help"
             obj.listCommands();
@@ -115,8 +114,8 @@ public class Client
             
         case 2:  //new flight
             if(arguments.size()!=5){
-	            obj.wrongNumber();
-	            break;
+            obj.wrongNumber();
+            break;
             }
             System.out.println("Adding a new Flight using id: "+arguments.elementAt(1));
             System.out.println("Flight number: "+arguments.elementAt(2));
@@ -124,30 +123,14 @@ public class Client
             System.out.println("Set Flight Price: "+arguments.elementAt(4));
             
             try{
-            	CURRENT_TRXN = rm.start();
-                
-	            Id = CURRENT_TRXN;
-	            flightNum = obj.getInt(arguments.elementAt(2));
-	            flightSeats = obj.getInt(arguments.elementAt(3));
-	            flightPrice = obj.getInt(arguments.elementAt(4));
-	            
-	            if(rm.addFlight(Id,flightNum,flightSeats,flightPrice))
-	            {
-	                if (rm.commit(CURRENT_TRXN))
-	                {
-	                	System.out.println("Flight added");
-	                }
-	                else
-	                {
-	                	rm.abort(CURRENT_TRXN);
-		                System.out.println("Flight could not be added. Please try again.");
-	                }
-            	}
-	            else
-	            {
-	                System.out.println("Flight could not be added");
-	            	rm.abort(CURRENT_TRXN);
-	            }
+            Id = obj.getInt(arguments.elementAt(1));
+            flightNum = obj.getInt(arguments.elementAt(2));
+            flightSeats = obj.getInt(arguments.elementAt(3));
+            flightPrice = obj.getInt(arguments.elementAt(4));
+            if(rm.addFlight(Id,flightNum,flightSeats,flightPrice))
+                System.out.println("Flight added");
+            else
+                System.out.println("Flight could not be added");
             }
             catch(Exception e){
             System.out.println("EXCEPTION:");
@@ -545,8 +528,7 @@ public class Client
             try{
             Id = obj.getInt(arguments.elementAt(1));
             int customer = obj.getInt(arguments.elementAt(2));
-            @SuppressWarnings("rawtypes")
-			Vector flightNumbers = new Vector();
+            Vector flightNumbers = new Vector();
             for(int i=0;i<arguments.size()-6;i++)
                 flightNumbers.addElement(arguments.elementAt(3+i));
             location = obj.getString(arguments.elementAt(arguments.size()-3));
@@ -575,20 +557,20 @@ public class Client
                         
         case 22:  //new Customer given id
             if(arguments.size()!=3){
-	            obj.wrongNumber();
-	            break;
+            obj.wrongNumber();
+            break;
             }
             System.out.println("Adding a new Customer using id:"+arguments.elementAt(1) + " and cid " +arguments.elementAt(2));
             try{
-	            Id = obj.getInt(arguments.elementAt(1));
-	            Cid = obj.getInt(arguments.elementAt(2));
-	            boolean customer=rm.newCustomer(Id,Cid);
-	            System.out.println("new customer id:"+Cid);
+            Id = obj.getInt(arguments.elementAt(1));
+            Cid = obj.getInt(arguments.elementAt(2));
+            boolean customer=rm.newCustomer(Id,Cid);
+            System.out.println("new customer id:"+Cid);
             }
             catch(Exception e){
-	            System.out.println("EXCEPTION:");
-	            System.out.println(e.getMessage());
-	            e.printStackTrace();
+            System.out.println("EXCEPTION:");
+            System.out.println(e.getMessage());
+            e.printStackTrace();
             }
             break;
             
@@ -596,307 +578,5 @@ public class Client
             System.out.println("The interface does not support this command.");
             break;
         }//end of switch
-        }//end of while(true)
-    }
-        
-    @SuppressWarnings({ "rawtypes", "unchecked" })
-	public Vector parse(String command)
-    {
-    Vector arguments = new Vector();
-    StringTokenizer tokenizer = new StringTokenizer(command,",");
-    String argument ="";
-    while (tokenizer.hasMoreTokens())
-        {
-        argument = tokenizer.nextToken();
-        argument = argument.trim();
-        arguments.add(argument);
-        }
-    return arguments;
-    }
-    public int findChoice(String argument)
-    {
-    if (argument.compareToIgnoreCase("help")==0)
-        return 1;
-    else if(argument.compareToIgnoreCase("newflight")==0)
-        return 2;
-    else if(argument.compareToIgnoreCase("newcar")==0)
-        return 3;
-    else if(argument.compareToIgnoreCase("newroom")==0)
-        return 4;
-    else if(argument.compareToIgnoreCase("newcustomer")==0)
-        return 5;
-    else if(argument.compareToIgnoreCase("deleteflight")==0)
-        return 6;
-    else if(argument.compareToIgnoreCase("deletecar")==0)
-        return 7;
-    else if(argument.compareToIgnoreCase("deleteroom")==0)
-        return 8;
-    else if(argument.compareToIgnoreCase("deletecustomer")==0)
-        return 9;
-    else if(argument.compareToIgnoreCase("queryflight")==0)
-        return 10;
-    else if(argument.compareToIgnoreCase("querycar")==0)
-        return 11;
-    else if(argument.compareToIgnoreCase("queryroom")==0)
-        return 12;
-    else if(argument.compareToIgnoreCase("querycustomer")==0)
-        return 13;
-    else if(argument.compareToIgnoreCase("queryflightprice")==0)
-        return 14;
-    else if(argument.compareToIgnoreCase("querycarprice")==0)
-        return 15;
-    else if(argument.compareToIgnoreCase("queryroomprice")==0)
-        return 16;
-    else if(argument.compareToIgnoreCase("reserveflight")==0)
-        return 17;
-    else if(argument.compareToIgnoreCase("reservecar")==0)
-        return 18;
-    else if(argument.compareToIgnoreCase("reserveroom")==0)
-        return 19;
-    else if(argument.compareToIgnoreCase("itinerary")==0)
-        return 20;
-    else if (argument.compareToIgnoreCase("quit")==0)
-        return 21;
-    else if (argument.compareToIgnoreCase("newcustomerid")==0)
-        return 22;
-    else
-        return 666;
-
-    }
-
-    public void listCommands()
-    {
-    System.out.println("\nWelcome to the client interface provided to test your project.");
-    System.out.println("Commands accepted by the interface are:");
-    System.out.println("help");
-    System.out.println("newflight\nnewcar\nnewroom\nnewcustomer\nnewcustomerid\ndeleteflight\ndeletecar\ndeleteroom");
-    System.out.println("deletecustomer\nqueryflight\nquerycar\nqueryroom\nquerycustomer");
-    System.out.println("queryflightprice\nquerycarprice\nqueryroomprice");
-    System.out.println("reserveflight\nreservecar\nreserveroom\nitinerary");
-    System.out.println("nquit");
-    System.out.println("\ntype help, <commandname> for detailed info(NOTE the use of comma).");
-    }
-
-
-    public void listSpecific(String command)
-    {
-    System.out.print("Help on: ");
-    switch(findChoice(command))
-        {
-        case 1:
-        System.out.println("Help");
-        System.out.println("\nTyping help on the prompt gives a list of all the commands available.");
-        System.out.println("Typing help, <commandname> gives details on how to use the particular command.");
-        break;
-
-        case 2:  //new flight
-        System.out.println("Adding a new Flight.");
-        System.out.println("Purpose:");
-        System.out.println("\tAdd information about a new flight.");
-        System.out.println("\nUsage:");
-        System.out.println("\tnewflight,<id>,<flightnumber>,<flightSeats>,<flightprice>");
-        break;
-        
-        case 3:  //new Car
-        System.out.println("Adding a new Car.");
-        System.out.println("Purpose:");
-        System.out.println("\tAdd information about a new car location.");
-        System.out.println("\nUsage:");
-        System.out.println("\tnewcar,<id>,<location>,<numberofcars>,<pricepercar>");
-        break;
-        
-        case 4:  //new Room
-        System.out.println("Adding a new Room.");
-        System.out.println("Purpose:");
-        System.out.println("\tAdd information about a new room location.");
-        System.out.println("\nUsage:");
-        System.out.println("\tnewroom,<id>,<location>,<numberofrooms>,<priceperroom>");
-        break;
-        
-        case 5:  //new Customer
-        System.out.println("Adding a new Customer.");
-        System.out.println("Purpose:");
-        System.out.println("\tGet the system to provide a new customer id. (same as adding a new customer)");
-        System.out.println("\nUsage:");
-        System.out.println("\tnewcustomer,<id>");
-        break;
-        
-        
-        case 6: //delete Flight
-        System.out.println("Deleting a flight");
-        System.out.println("Purpose:");
-        System.out.println("\tDelete a flight's information.");
-        System.out.println("\nUsage:");
-        System.out.println("\tdeleteflight,<id>,<flightnumber>");
-        break;
-        
-        case 7: //delete Car
-        System.out.println("Deleting a Car");
-        System.out.println("Purpose:");
-        System.out.println("\tDelete all cars from a location.");
-        System.out.println("\nUsage:");
-        System.out.println("\tdeletecar,<id>,<location>,<numCars>");
-        break;
-        
-        case 8: //delete Room
-        System.out.println("Deleting a Room");
-        System.out.println("\nPurpose:");
-        System.out.println("\tDelete all rooms from a location.");
-        System.out.println("Usage:");
-        System.out.println("\tdeleteroom,<id>,<location>,<numRooms>");
-        break;
-        
-        case 9: //delete Customer
-        System.out.println("Deleting a Customer");
-        System.out.println("Purpose:");
-        System.out.println("\tRemove a customer from the database.");
-        System.out.println("\nUsage:");
-        System.out.println("\tdeletecustomer,<id>,<customerid>");
-        break;
-        
-        case 10: //querying a flight
-        System.out.println("Querying flight.");
-        System.out.println("Purpose:");
-        System.out.println("\tObtain Seat information about a certain flight.");
-        System.out.println("\nUsage:");
-        System.out.println("\tqueryflight,<id>,<flightnumber>");
-        break;
-        
-        case 11: //querying a Car Location
-        System.out.println("Querying a Car location.");
-        System.out.println("Purpose:");
-        System.out.println("\tObtain number of cars at a certain car location.");
-        System.out.println("\nUsage:");
-        System.out.println("\tquerycar,<id>,<location>");        
-        break;
-        
-        case 12: //querying a Room location
-        System.out.println("Querying a Room Location.");
-        System.out.println("Purpose:");
-        System.out.println("\tObtain number of rooms at a certain room location.");
-        System.out.println("\nUsage:");
-        System.out.println("\tqueryroom,<id>,<location>");        
-        break;
-        
-        case 13: //querying Customer Information
-        System.out.println("Querying Customer Information.");
-        System.out.println("Purpose:");
-        System.out.println("\tObtain information about a customer.");
-        System.out.println("\nUsage:");
-        System.out.println("\tquerycustomer,<id>,<customerid>");
-        break;               
-        
-        case 14: //querying a flight for price 
-        System.out.println("Querying flight.");
-        System.out.println("Purpose:");
-        System.out.println("\tObtain price information about a certain flight.");
-        System.out.println("\nUsage:");
-        System.out.println("\tqueryflightprice,<id>,<flightnumber>");
-        break;
-        
-        case 15: //querying a Car Location for price
-        System.out.println("Querying a Car location.");
-        System.out.println("Purpose:");
-        System.out.println("\tObtain price information about a certain car location.");
-        System.out.println("\nUsage:");
-        System.out.println("\tquerycarprice,<id>,<location>");        
-        break;
-        
-        case 16: //querying a Room location for price
-        System.out.println("Querying a Room Location.");
-        System.out.println("Purpose:");
-        System.out.println("\tObtain price information about a certain room location.");
-        System.out.println("\nUsage:");
-        System.out.println("\tqueryroomprice,<id>,<location>");        
-        break;
-
-        case 17:  //reserve a flight
-        System.out.println("Reserving a flight.");
-        System.out.println("Purpose:");
-        System.out.println("\tReserve a flight for a customer.");
-        System.out.println("\nUsage:");
-        System.out.println("\treserveflight,<id>,<customerid>,<flightnumber>");
-        break;
-        
-        case 18:  //reserve a car
-        System.out.println("Reserving a Car.");
-        System.out.println("Purpose:");
-        System.out.println("\tReserve a given number of cars for a customer at a particular location.");
-        System.out.println("\nUsage:");
-        System.out.println("\treservecar,<id>,<customerid>,<location>,<nummberofCars>");
-        break;
-        
-        case 19:  //reserve a room
-        System.out.println("Reserving a Room.");
-        System.out.println("Purpose:");
-        System.out.println("\tReserve a given number of rooms for a customer at a particular location.");
-        System.out.println("\nUsage:");
-        System.out.println("\treserveroom,<id>,<customerid>,<location>,<nummberofRooms>");
-        break;
-        
-        case 20:  //reserve an Itinerary
-        System.out.println("Reserving an Itinerary.");
-        System.out.println("Purpose:");
-        System.out.println("\tBook one or more flights.Also book zero or more cars/rooms at a location.");
-        System.out.println("\nUsage:");
-        System.out.println("\titinerary,<id>,<customerid>,<flightnumber1>....<flightnumberN>,<LocationToBookCarsOrRooms>,<NumberOfCars>,<NumberOfRoom>");
-        break;
-        
-
-        case 21:  //quit the client
-        System.out.println("Quitting client.");
-        System.out.println("Purpose:");
-        System.out.println("\tExit the client application.");
-        System.out.println("\nUsage:");
-        System.out.println("\tquit");
-        break;
-        
-        case 22:  //new customer with id
-            System.out.println("Create new customer providing an id");
-            System.out.println("Purpose:");
-            System.out.println("\tCreates a new customer with the id provided");
-            System.out.println("\nUsage:");
-            System.out.println("\tnewcustomerid, <id>, <customerid>");
-            break;
-
-        default:
-        System.out.println(command);
-        System.out.println("The interface does not support this command.");
-        break;
-        }
-    }
-    
-    public void wrongNumber() {
-    System.out.println("The number of arguments provided in this command are wrong.");
-    System.out.println("Type help, <commandname> to check usage of this command.");
-    }
-
-
-
-    public int getInt(Object temp) throws Exception {
-    try {
-        return (new Integer((String)temp)).intValue();
-        }
-    catch(Exception e) {
-        throw e;
-        }
-    }
-    
-    public boolean getBoolean(Object temp) throws Exception {
-        try {
-            return (new Boolean((String)temp)).booleanValue();
-            }
-        catch(Exception e) {
-            throw e;
-            }
-    }
-
-    public String getString(Object temp) throws Exception {
-    try {    
-        return (String)temp;
-        }
-    catch (Exception e) {
-        throw e;
-        }
     }
 }
